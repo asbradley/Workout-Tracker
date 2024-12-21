edd8d7639eaeda653050ed08da5131d6af355847
aidanbradley <bradl154@purdue.edu>
2024-12-21 12:31:06 -0500

    Initial Commit

---
+++ Workout Tracker/Workout_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_TrackerApp.swift
+//  Workout Tracker
+//
+//  Created by Aidan Bradley on 12/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Workout_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
